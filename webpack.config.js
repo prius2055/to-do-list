4fb45eb06ca1498a0a8792e791c446a46a622395
prius2055 <prius2055@gmail.com>
2023-03-28 23:08:35 +0100

    Create new branch. Set up webpack bundle: Install css loader. Install html loader dependency. Install dependency for development

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
