f8ed89ffac0bee9b2e44628b6e65b24a0d8dc041
prius2055 <prius2055@gmail.com>
2023-03-30 00:07:32 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
-import Todos from "../modules/Todo.js";
-import "./style.css";
-import TurnLeft from "./img/turn-left.png";
+import Todos from '../modules/Todo.js';
+import './style.css';
+import TurnLeft from './img/turn-left.png';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const todoInput = document.querySelector(".todo-input");
-  const newTodoInput = document.querySelector(".new-todo-input");
-  // const todoList = document.querySelector("todo-list");
+document.addEventListener('DOMContentLoaded', () => {
+  const todoInput = document.querySelector('.todo-input');
+  const newTodoInput = document.querySelector('.new-todo-input');
 
-  const img = document.createElement("img");
+  const img = document.createElement('img');
   img.src = `${TurnLeft}`;
-  img.alt = "turn left";
+  img.alt = 'turn left';
   todoInput.appendChild(img);
 
-  // const todoList = document.querySelector("todo-list");
   const todo = new Todos();
   const description = newTodoInput.value;
   if (description) {
     todo.addTodo(description);
-    description.value = "";
+    description.value = '';
   }
   todo.displayTodo();
 
-  newTodoInput.addEventListener("change", (e) => {
+  newTodoInput.addEventListener('change', (e) => {
     e.preventDefault();
 
     const todo = new Todos();
     todo.addTodo(e.target.value);
-    newTodoInput.value = "";
+    newTodoInput.value = '';
   });
 });
-
